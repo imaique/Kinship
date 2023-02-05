7f7f3a955e94b87922fc52a56667ce692811a937
Ali Taladar <taladarali@gmail.com>
2023-02-04 22:33:26 -0500

    Added recognition.py

--- api/api.py
+++ api/api.py
@@ -4,12 +4,14 @@ import base64
 
 app = FastAPI()
 
+
 class UserInfo(BaseModel):
     Name: str
     Image: str
 
+
 @app.post('/')
-async def upload_info_endpoint(userinfo:UserInfo):
+async def upload_info_endpoint(userinfo: UserInfo):
     img_data = bytes(userinfo.Image, encoding="ascii")
     with open("imageToSave.png", "wb") as fh:
         fh.write(base64.decodebytes(img_data))
