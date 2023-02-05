a725684e57840316d231ec2120d2e0573df264ba
Louri Compain <louri.compain@gmail.com>
2023-02-04 23:19:32 -0500

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import base64
-
+from recognition import FaceRecognition
+face_recogniser = FaceRecognition()
 app = FastAPI()
 
 
@@ -13,7 +14,8 @@ class UserInfo(BaseModel):
 @app.post('/')
 async def upload_info_endpoint(userinfo: UserInfo):
     img_data = bytes(userinfo.Image, encoding="ascii")
-    with open("imageToSave.png", "wb") as fh:
-        fh.write(base64.decodebytes(img_data))
+    # with open("imageToSave.png", "wb") as fh:
+    #     fh.write(base64.decodebytes(img_data))
+    face_recogniser.add_face(img_data)
 
     return userinfo
