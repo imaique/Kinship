788364b1e9db87de6b9b52144a22f1f4650801c4
Michael <osujim2012@gmail.com>
2023-02-04 21:59:46 -0500

    Add start of api

---
+++ api/api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import base64
+
+app = FastAPI()
+
+class UserInfo(BaseModel):
+    Name: str
+    Image: str
+
+@app.post('/')
+async def upload_info_endpoint(userinfo:UserInfo):
+    img_data = bytes(userinfo.Image, encoding="ascii")
+    with open("imageToSave.png", "wb") as fh:
+        fh.write(base64.decodebytes(img_data))
+
+    return userinfo
